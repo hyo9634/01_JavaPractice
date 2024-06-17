b137862dd382de0914ca54cee4204f535aad26ea
DESKTOP-CIKGCIA\ㅇㅅㅇ <dlgytmd5468@gmail.com>
2024-06-17 09:11:03 +0900

    20240617 javapractice

---
+++ 01-java-practice/src/javapractice/day20240614/Question10.java
@@ -0,0 +1,21 @@
+package javapractice.day20240614;
+
+import java.util.Scanner;
+
+public class Question10 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+
+        if ((a > c) && (b > d)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
---
+++ 01-java-practice/src/javapractice/day20240614/Question9.java
@@ -0,0 +1,27 @@
+package javapractice.day20240614;
+
+//두 정수를 입력받아 10 15
+//첫 번째 수는 전치증가연산자 사용
+//두 번째 수는 후치감소연산자를 사용하여 출력
+//바뀐 값을 다시 출력
+
+import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a;
+        int b;
+        a = scan.nextInt();
+        b = scan.nextInt();
+
+        System.out.print(++a);
+        System.out.println(" " + b--);
+        System.out.print(a);
+        System.out.print(" " + b--);
+
+
+
+    }
+    }
+
